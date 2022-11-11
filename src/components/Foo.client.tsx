3b965c1809cb2a7d56d7da244a5f7d3c6847bef4
Isaac Chavez <chavez.isaac93@gmail.com>
2022-11-10 18:50:11 -0800

    Make some updates to get a working data set

--- src/components/Foo.client.tsx
+++ src/components/Foo.client.tsx
@@ -1,93 +1,104 @@
 import { useState } from 'react';
 import styles from './index.module.scss';
 
-const prices = {
-  subscribe: {
-    qty: [
-    {
-      label: 1,
-      strikethrough: '39.99',
-      price: 34.99,
-    },
-    {
-      label: 2,
-      strikethrough: '79.98',
-      price: 63.98,
-    },
-    {
-      label: 3,
-      strikethrough: '120',
-      price: 90,
-    },
-  ]},
-  onetime: {
-    qty: [
-    {
-      label: 1,
-      strikethrough: '',
-      price: 39.99,
-    },
-    {
-      label: 2,
-      price: 69.98,
-    },
-    {
-      label: 3,
-      price: 99.00,
-    },
-  ]}
-}
 
 export default function Foo() {
 
-  const [purchaseType, setPurchaseType] = useState("subscribe");
- 
-  const onChangePurchaseType = () => {
-
+  interface ShopStates{
+    [index: number]: ProductSelectionData,
   }
+
+  interface ProductSelectionData{
+    purchaseType: string,
+    itemQuantity: number,
+    strikethrough: string,
+    price: number
+  }
+
+  const shopData: ShopStates = [
+      {
+        purchaseType: 'subscribe',
+        itemQuantity: 1,
+        strikethrough: '39.99',
+        price: 34.99,
+      },{
+        purchaseType: 'subscribe',
+        itemQuantity: 2,
+        strikethrough: '79.98',
+        price: 63.98,
+      },{
+        purchaseType: 'subscribe',
+        itemQuantity: 3,
+        strikethrough: '120',
+        price: 90,
+      },{
+        purchaseType: 'onetime',
+        itemQuantity: 1,
+        strikethrough: '',
+        price: 39.99,
+      },{
+        purchaseType: 'onetime',
+        itemQuantity: 2,
+        strikethrough: '79.98',
+        price: 69.98,
+      },{
+        purchaseType: 'onetime',
+        itemQuantity: 3,
+        strikethrough: '120',
+        price: 99.00,
+      }
+  ]
+  const [purchaseType, setPurchaseType] = useState("subscribe");
+  const [itemQuantity, setItemQuantity] = useState(1);
+
+  const shopDataArray = shopData as Array<any>;
+  let selectionData =  shopDataArray.find(price => (price.purchaseType === purchaseType && price.itemQuantity === itemQuantity));
+  let alternativeData =  shopDataArray.find(price => (price.purchaseType !== purchaseType && price.itemQuantity === itemQuantity));
   
+  console.log(alternativeData.price)
+
   return <div className={styles.wrapper}>
-    
+
     <h2>1. Select Purchase Type</h2>
     <div>
-      <input type="radio" id="subscribe" name='type' onChange={() => setPurchaseType("subscribe")} checked={purchaseType === "subscribe"}/>
-     <label htmlFor="subscribe">
-      <div>
-      <h3>Subscribe & Save</h3>
-        <span>Easy to cancel, anytime</span>
-        <span><strong>Free Shipping Always</strong></span>
-        <span className='price'>$34.99</span>
-        <span className='unit'>/BOTTLE</span>
+      <input type="radio" id="subscribe" name='type' onChange={() => setPurchaseType("subscribe")} checked={purchaseType === "subscribe"} />
+      <label htmlFor="subscribe">
+        <div>
+          <h3>Subscribe & Save</h3>
+          <span>Easy to cancel, anytime</span>
+          <span><strong>Free Shipping Always</strong></span>
+          <span className='price'>${}</span>
+          <span className='unit'>/BOTTLE</span>
 
-      </div>
-      </label> 
+        </div>
+      </label>
     </div>
     <div>
-      <input type="radio" id="onetime" name='type' onChange={() => setPurchaseType("onetime")} checked={purchaseType === "onetime"}/>
+      <input type="radio" id="onetime" name='type' onChange={() => setPurchaseType("onetime")} checked={purchaseType === "onetime"} />
       <label htmlFor="onetime">
         <div>
-        <h3>One Time</h3>
-        <span>One Time Purchase</span>
-        <span><strong>Free Shipping Always</strong></span>
-        <span className='price'>$39.99</span>
-        <span className='unit'>/BOTTLE</span>
+          <h3>One Time</h3>
+          <span>One Time Purchase</span>
+          <span><strong>Free Shipping Always</strong></span>
+          <span className='price'>$39.99</span>
+          <span className='unit'>/BOTTLE</span>
         </div>
       </label>
     </div>
     <h2>2. Quantity</h2>
     <div>
-      <input type="radio" id="one" name='qty'/>
+      <input type="radio" id="one" name='qty' onChange={() => setItemQuantity(1)} checked={itemQuantity === 1} />
       <label htmlFor="one">1</label>
     </div>
     <div>
-      <input type="radio" id="two" name='qty'/>
+      <input type="radio" id="two" name='qty' onChange={() => setItemQuantity(2)} checked={itemQuantity === 2} />
       <label htmlFor="two">2</label>
     </div>
     <div>
-      <input type="radio" id="three" name='qty'/>
+      <input type="radio" id="three" name='qty' onChange={() => setItemQuantity(3)} checked={itemQuantity === 3} />
       <label htmlFor="three">3</label>
     </div>
 
-    <button>Add to Cart - 39.99</button>
+    <button>Add to Cart - {selectionData.price}</button>
   </div>;
 }
