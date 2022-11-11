3d328cf05d819cf1c1a5721a047394b0f503759a
Isaac Chavez <chavez.isaac93@gmail.com>
2022-11-10 20:58:40 -0800

    Added more data

--- src/components/Foo.client.tsx
+++ src/components/Foo.client.tsx
@@ -12,40 +12,54 @@ export default function Foo() {
     purchaseType: string,
     itemQuantity: number,
     strikethrough: string,
-    price: number
+    pricePerUnit: string,
+    unitLabel: string,
+    price: string
   }
 
   const shopData: ShopStates = [
       {
         purchaseType: 'subscribe',
         itemQuantity: 1,
-        strikethrough: '39.99',
-        price: 34.99,
+        strikethrough: '$39.99',
+        pricePerUnit: '$34.99',
+        unitLabel: '/BOTTLE',
+        price: '$34.99',
       },{
         purchaseType: 'subscribe',
         itemQuantity: 2,
-        strikethrough: '79.98',
-        price: 63.98,
+        strikethrough: '$79.98',
+        pricePerUnit: '$31.99',
+        unitLabel: '/BOTTLE',
+        price: '$63.98',
       },{
         purchaseType: 'subscribe',
         itemQuantity: 3,
-        strikethrough: '120',
-        price: 90,
+        strikethrough: '$120',
+        pricePerUnit: '$30',
+        unitLabel: '/BOTTLE',
+        price: '$90',
       },{
         purchaseType: 'onetime',
         itemQuantity: 1,
         strikethrough: '',
-        price: 39.99,
+        pricePerUnit: '$39.99',
+        unitLabel: '/BOTTLE',
+        price: '$39.99',
       },{
         purchaseType: 'onetime',
         itemQuantity: 2,
-        strikethrough: '79.98',
-        price: 69.98,
+        strikethrough: '$79.98',
+        pricePerUnit: '$34.99',
+        unitLabel: '/BOTTLE',
+        price: '$69.98',
       },{
         purchaseType: 'onetime',
         itemQuantity: 3,
-        strikethrough: '120',
-        price: 99.00,
+        strikethrough: '$120',
+        pricePerUnit: '$33',
+        unitLabel: '/BOTTLE',
+        price: '$99',
       }
   ]
   const [purchaseType, setPurchaseType] = useState<string>("subscribe");
@@ -55,7 +69,6 @@ export default function Foo() {
   let selectionData =  (aPurchaseType, aQuantity) => {return shopDataArray.find(price => (price.purchaseType === aPurchaseType && price.itemQuantity === aQuantity));}
 
   return <div className={styles.wrapper}>
-
     <h2>1. Select Purchase Type</h2>
     <div>
       <input type="radio" id="subscribe" name='type' onChange={() => setPurchaseType("subscribe")} checked={purchaseType === "subscribe"} />
@@ -64,8 +77,8 @@ export default function Foo() {
           <h3>Subscribe & Save</h3>
           <span>Easy to cancel, anytime</span>
           <span><strong>Free Shipping Always</strong></span>
-          <span className='price'>${selectionData("subscribe", itemQuantity).price/itemQuantity}</span>
-          <span className='unit'>/BOTTLE</span>
+          <span className='price'>{selectionData("subscribe", itemQuantity).pricePerUnit}</span>
+          <span className='unit'>{selectionData("subscribe", itemQuantity).unitLabel}</span>
 
         </div>
       </label>
@@ -77,8 +90,8 @@ export default function Foo() {
           <h3>One Time</h3>
           <span>One Time Purchase</span>
           <span><strong>Free Shipping Always</strong></span>
-          <span className='price'>${selectionData("onetime", itemQuantity).price/itemQuantity}</span>
-          <span className='unit'>/BOTTLE</span>
+          <span className='price'>{selectionData("onetime", itemQuantity).pricePerUnit}</span>
+          <span className='unit'>{selectionData("onetime", itemQuantity).unitLabel}</span>
         </div>
       </label>
     </div>
@@ -96,6 +109,7 @@ export default function Foo() {
       <label htmlFor="three">3</label>
     </div>
 
-    <button>Add to Cart - {selectionData(purchaseType, itemQuantity).price}</button>
+    <button onClick={() => alert("Hello")} className={styles.button}>Add to Cart - {<span className={styles.strikethrough}>{selectionData(purchaseType, itemQuantity).strikethrough}</span>} {selectionData(purchaseType, itemQuantity).price}</button>
+
   </div>;
 }
